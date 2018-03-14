b00ab05903c4514630b8b2c19dd8a5d0fa6e392a
fuyuda <704337316@qq.com>
2018-03-14 23:44:37 +0800

    初始化

---
+++ mindhelper-web/src/main/java/com/fuyuda/Application.java
@@ -0,0 +1,19 @@
+package com.fuyuda;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+@SpringBootApplication
+@ComponentScan
+@EnableScheduling
+@MapperScan("com.fuyuda.**.mapper")
+@EnableTransactionManagement
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
+
